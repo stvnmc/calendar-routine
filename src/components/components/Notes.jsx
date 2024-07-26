208671e3914e01735837b8a6df33044c1bcf1e68
stvnmc <stvnmc123@gmail.com>
2024-07-25 21:29:23 -0600

    button add ideas

--- src/components/components/Notes.jsx
+++ src/components/components/Notes.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { SlArrowDown } from "react-icons/sl";
+import { IoMdAdd } from "react-icons/io";
 
 const Notes = ({ openIdeas, setOpenIdeas }) => {
+  const [openCreateIdeas, setOpenCreateIdeas] = useState(false);
+
   const notes = [
     "Dsad a ssdDsa dassdD sada ssdDsa da ssd Dsadassd Dsadass dDsadas sdDsadassd",
     "dsadas",
@@ -18,21 +21,43 @@ const Notes = ({ openIdeas, setOpenIdeas }) => {
     setOpenIdeas(!openIdeas);
   };
 
+  const chanceOpenCreateIdeas = () => {
+    setOpenCreateIdeas(true);
+    setOpenIdeas(true);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target)
+  };
+
   return (
     <div className={`ideas ${openIdeas ? "open" : ""}`}>
       <div className="ideas-top">
         <h2>Ideas</h2>
-
-        <button onClick={chanceOpenIdeas}>
-          <SlArrowDown />
-        </button>
+        <div className="cont-button">
+          <button onClick={chanceOpenCreateIdeas}>
+            <IoMdAdd />
+          </button>
+          <button className="open" onClick={chanceOpenIdeas}>
+            <SlArrowDown />
+          </button>
+        </div>
       </div>
       {openIdeas ? (
-        <div className="ideas-body">
-          {notes.map((item, i) => (
-            <p key={i}>{item}</p>
-          ))}
-        </div>
+        openCreateIdeas ? (
+          <form onSubmit={onSubmit}>
+            <label>add idea</label>
+            <input type="text" placeholder="ideas" />
+            <button>add</button>
+          </form>
+        ) : (
+          <div className="ideas-body">
+            {notes.map((item, i) => (
+              <p key={i}>{item}</p>
+            ))}
+          </div>
+        )
       ) : (
         ""
       )}
